a0141c8189ed4b772b55d0b5360f19f463641de3
chaudutenomad <bichdu@tenomad.com>
2023-09-28 16:45:35 +0700

    add swagger

---
+++ config/database.js
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'mysql',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'root'),
+      password: env('DATABASE_PASSWORD', 'du3001'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), 
+      },
+    },
+    debug: false,
+  },
+});
